26f2af4bde6e0f655909e702baead84f820758f8
Kuurow <kuuroww@gmail.com>
2024-05-03 01:24:04 +0200

    changed amount of pictures

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -5,7 +5,7 @@ import addBluredDataUrls from "@/lib/getBase64";
 import env from "@/lib/env"
 
 export default async function Gallery() {
-    const url = `https://api.cloudinary.com/v1_1/${env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=20`;
+    const url = `https://api.cloudinary.com/v1_1/${env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=50`;
     const images: ImagesResults | undefined = await fetchImages(url);
 
     if (!images) return <h2 className="m-4 text-2xl font-bald text-white h-dvh text-center">No images found</h2>
